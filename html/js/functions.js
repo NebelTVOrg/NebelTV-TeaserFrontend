43f87f3c8879c40ec06969f720f32a99cfbedd56
Aleksey Baidan <howtwizer@gmail.com>
2014-01-05 11:57:40 +0200

    v 0.0.2

--- html/js/functions.js
+++ html/js/functions.js
@@ -2,7 +2,7 @@
  * Created by howtwizer on 23.12.13.
  */
 function contentheight(){
-    var contentheight = $(document).height() - 76;
+    var contentheight = $(document).height();
     $('.content').height( contentheight );
 }
 
@@ -23,16 +23,19 @@ $(document).ready(function(){
         },
         function(data) {
             var feedList = [];
+
+
             $(data).each(function(index, feedItem) {
                 feedList.push(
                     '<li class="item">\
                         <div class="feed-item-info">\
+                        <a href="http://mirrorblender.top-ix.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_h264.mov"><img src="http://www.bigbuckbunny.org/wp-content/uploads/dl_480p.jpg" alt="" /></a>\
 						<h5>'+feedItem.title+'</h5>\
 						<span class="feed-item-instant">Instant</span>\
 						<span class="feed-item-time">02:45:07</span>\
 						<span class="feed-item-imdb">'+((Math.random()*10).toFixed(1))+'</span>\
 						<span class="feed-item-rating"><span style="width: '+((Math.random()*100).toFixed(1))+'%"></span></span>\
-						<div class="feed-item-descr">'+feedItem.descr+' ЗДЕСЬ ОПИСАНИЕ МЕДИА КОНТЕНТА Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>\
+						<div class="feed-item-descr">'+feedItem.descr+'</div>\
 						<div class="feed-item-author-info">\
 							<span class="feed-item-author">Витя Охрименко '+feedItem.author+'</span>\
 							<span class="feed-item-date">'+feedItem.date+'</span>\
@@ -66,6 +69,36 @@ $(document).ready(function(){
 					</div>\
 				</li>');
             });
+
+            var resentList = [];
+
+            $(data).each(function(index, feedItem) {
+                resentList.push(
+                    '<li class="item" style="background: url(\' http://www.bigbuckbunny.org/wp-content/uploads/dl_480p.jpg \') 100%/100% no-repeat;">\
+                        <div class="coverbg" ></div>\
+                        <div class="feed-item-info">\
+						<h5>'+feedItem.title+'</h5>\
+						<span class="feed-item-instant">Instant</span>\
+						<span class="feed-item-time">12:45:07</span>\
+						<span class="feed-item-imdb">'+((Math.random()*10).toFixed(1))+'</span>\
+						<span class="feed-item-rating"><span style="width: '+((Math.random()*100).toFixed(1))+'%"></span></span>\
+						<div class="feed-item-descr">'+feedItem.descr+'</div>\
+						<div class="feed-item-author-info">\
+							<span class="feed-item-author">Витя Охрименко '+feedItem.author+'</span>\
+							<span class="feed-item-date">'+feedItem.date+'</span>\
+							<div>Здесь сообщение от френда, который поделился этим медиа контентом Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</div>\
+							<img class="feed-item-author-photo" src="https://pp.vk.me/c313225/v313225380/2bd4/cnVjs4x6oO0.jpg">\
+						</div>\
+						<div class="feed-item-bottom">\
+							<div class="feed-item-bottom-right">\
+								<span class="feed-item-icon-likes">'+(Math.floor(Math.random()*1000))+'</span>\
+								<span class="feed-item-icon-comments">'+(Math.floor(Math.random()*100))+'</span>\
+							</div>\
+							<span>Tags: ● San diego, ● David koechner</span>\
+						</div>\
+					</div></li>');
+            });
+
             //alert (feedList);
 
             $('#feedList').append(feedList);
@@ -74,7 +107,7 @@ $(document).ready(function(){
             $('.feed-item-bottom').click(function(){
                 $(this).closest('li').find('.feed-item-comments').toggle();
             });
-            $('#resentList').append(feedList);
+            $('#resentList').append(resentList);
            // feedList.appendTo('#someList');
         }
     );
