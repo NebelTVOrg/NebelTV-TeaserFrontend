1061b50ecd81e9819ef5946694fd2018f18a0a3b
Antonio Shlapak <antonio.shlapak@gmail.com>
2014-04-25 01:14:04 +0300

    Minor: update media link (remote temporary) front-end config: add all menu references

--- html/js/video.js
+++ html/js/video.js
@@ -95,7 +95,7 @@ function builder(data) {
                     </div>\
                     <div class="item-body">\
                               <img  class="img-responsive" src="'+imageLink+'">\
-                             <a class="play" href="http://nebel.tv/IvaWrapperWeb/getVideoAssets?id=7"><span class="glyphicon glyphicon-play"></span></a>\
+                             <a class="play" href="http://54.201.170.111/assets/001-720p-2500kb.mp4"><span class="glyphicon glyphicon-play"></span></a>\
                     </div>\
                     <div class="item-footer">\
                         <div class="video-title row">\
