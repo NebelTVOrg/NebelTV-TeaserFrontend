91d7dc72ec308ba3a9ff5c69baf9c527a9536d25
Aleksey Baidan <howtwizer@gmail.com>
2014-05-05 09:16:39 +0300

    Offline version

---
+++ html/js/sport-item.js
@@ -0,0 +1,179 @@
+/**
+ * Created by howtwizer on 23.12.13.
+ */
+
+/* Define Host
+ * http://54.201.170.111:8080/IvaWrapperWeb - development version (browser testing)
+ *
+ * */
+//var host = 'http://54.201.170.111:8080/IvaWrapperWeb';
+var data;
+data = {"version":"1.0","encoding":"UTF-8","feed":{"xmlns":"http://www.w3.org/2005/Atom","xmlns$openSearch":"http://a9.com/-/spec/opensearchrss/1.0/","xmlns$gsx":"http://schemas.google.com/spreadsheets/2006/extended","id":{"$t":"https://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values"},"updated":{"$t":"2014-05-04T18:30:36.506Z"},"category":[{"scheme":"http://schemas.google.com/spreadsheets/2006","term":"http://schemas.google.com/spreadsheets/2006#list"}],"title":{"type":"text","$t":"Sport FACE Feed"},"link":[{"rel":"alternate","type":"text/html","href":"https://spreadsheets.google.com/pub?key\u003d0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc"},{"rel":"http://schemas.google.com/g/2005#feed","type":"application/atom+xml","href":"http://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values"},{"rel":"self","type":"application/atom+xml","href":"http://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values?alt\u003djson"}],"author":[{"name":{"$t":"anton.yacenko"},"email":{"$t":"anton.yacenko@gmail.com"}}],"openSearch$totalResults":{"$t":"5"},"openSearch$startIndex":{"$t":"1"},"entry":[{"id":{"$t":"https://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/d6ua4"},"updated":{"$t":"2014-05-04T18:30:36.506Z"},"category":[{"scheme":"http://schemas.google.com/spreadsheets/2006","term":"http://schemas.google.com/spreadsheets/2006#list"}],"title":{"type":"text","$t":"NHL Games"},"content":{"type":"text","$t":"coverimage: http://images.military.com/media/offduty/games/nhl13.jpg, tags: Sport, notes: that’s not a show, maybe you mean \"All stars match\" per some period, commentsauthor: Ivory, comment: adipiscing fringilla, porttitor vulputate, posuere vulputate, lacus. Cras interdum. Nunc sollicitudin commodo, faces: snowwrite.jpg, haveseries: 0, coverfile: nhl13.jpg"},"link":[{"rel":"self","type":"application/atom+xml","href":"http://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/d6ua4"}],"gsx$name":{"$t":"NHL Games"},"gsx$episodetimingaverage":{"$t":""},"gsx$description":{"$t":""},"gsx$source":{"$t":""},"gsx$coverimage":{"$t":"http://images.military.com/media/offduty/games/nhl13.jpg"},"gsx$imdbrating":{"$t":""},"gsx$broadcast":{"$t":""},"gsx$wikipage":{"$t":""},"gsx$imdbpage":{"$t":""},"gsx$tags":{"$t":"Sport"},"gsx$originalrun":{"$t":""},"gsx$notes":{"$t":"that’s not a show, maybe you mean \"All stars match\" per some period"},"gsx$commentsauthor":{"$t":"Ivory"},"gsx$comment":{"$t":"adipiscing fringilla, porttitor vulputate, posuere vulputate, lacus. Cras interdum. Nunc sollicitudin commodo"},"gsx$faces":{"$t":"snowwrite.jpg"},"gsx$haveseries":{"$t":"0"},"gsx$coverfile":{"$t":"nhl13.jpg"}},{"id":{"$t":"https://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/d88ul"},"updated":{"$t":"2014-05-04T18:30:36.506Z"},"category":[{"scheme":"http://schemas.google.com/spreadsheets/2006","term":"http://schemas.google.com/spreadsheets/2006#list"}],"title":{"type":"text","$t":"NFL Game Day"},"content":{"type":"text","$t":"coverimage: http://www.bodybuilding.com/fun/images/2012/nfl-game-day-2012-get-off-your-tailgate.jpg, tags: Sport, notes: same as above, commentsauthor: Amelia, comment: erat vitae risus. Duis a mi fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat nonummy ultricies ornare,, faces: vipulnsward.jpg, haveseries: 0, coverfile: nfl-game-day-2012-get-off-your-tailgate.jpg"},"link":[{"rel":"self","type":"application/atom+xml","href":"http://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/d88ul"}],"gsx$name":{"$t":"NFL Game Day"},"gsx$episodetimingaverage":{"$t":""},"gsx$description":{"$t":""},"gsx$source":{"$t":""},"gsx$coverimage":{"$t":"http://www.bodybuilding.com/fun/images/2012/nfl-game-day-2012-get-off-your-tailgate.jpg"},"gsx$imdbrating":{"$t":""},"gsx$broadcast":{"$t":""},"gsx$wikipage":{"$t":""},"gsx$imdbpage":{"$t":""},"gsx$tags":{"$t":"Sport"},"gsx$originalrun":{"$t":""},"gsx$notes":{"$t":"same as above"},"gsx$commentsauthor":{"$t":"Amelia"},"gsx$comment":{"$t":"erat vitae risus. Duis a mi fringilla mi lacinia mattis. Integer eu lacus. Quisque imperdiet, erat nonummy ultricies ornare,"},"gsx$faces":{"$t":"vipulnsward.jpg"},"gsx$haveseries":{"$t":"0"},"gsx$coverfile":{"$t":"nfl-game-day-2012-get-off-your-tailgate.jpg"}},{"id":{"$t":"https://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/dmair"},"updated":{"$t":"2014-05-04T18:30:36.506Z"},"category":[{"scheme":"http://schemas.google.com/spreadsheets/2006","term":"http://schemas.google.com/spreadsheets/2006#list"}],"title":{"type":"text","$t":"A Football Life"},"content":{"type":"text","$t":"episodetimingaverage: 45min, description: Profiles the most important and influential figures in the history of the National Football League., coverimage: http://upload.wikimedia.org/wikipedia/en/6/67/A_Football_Life_Season_One_DVD.jpg, imdbrating: 8,6, broadcast: NFL Network, wikipage: http://en.wikipedia.org/wiki/A_Football_Life, imdbpage: http://www.imdb.com/title/tt2396758/?ref_\u003dfn_tt_tt_1, tags: Biography, Sport, football, NFL, originalrun: September 15, 2011 – Present, (3 seasons), commentsauthor: Alexa, comment: lacinia mattis. Integer eu lacus. Quisque imperdiet, erat, faces: bobbytwoshoes.jpg, haveseries: 0, coverfile: A_Football_Life_Season_One_DVD.jpg"},"link":[{"rel":"self","type":"application/atom+xml","href":"http://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/dmair"}],"gsx$name":{"$t":"A Football Life"},"gsx$episodetimingaverage":{"$t":"45min"},"gsx$description":{"$t":"Profiles the most important and influential figures in the history of the National Football League."},"gsx$source":{"$t":""},"gsx$coverimage":{"$t":"http://upload.wikimedia.org/wikipedia/en/6/67/A_Football_Life_Season_One_DVD.jpg"},"gsx$imdbrating":{"$t":"8,6"},"gsx$broadcast":{"$t":"NFL Network"},"gsx$wikipage":{"$t":"http://en.wikipedia.org/wiki/A_Football_Life"},"gsx$imdbpage":{"$t":"http://www.imdb.com/title/tt2396758/?ref_\u003dfn_tt_tt_1"},"gsx$tags":{"$t":"Biography, Sport, football, NFL"},"gsx$originalrun":{"$t":"September 15, 2011 – Present, (3 seasons)"},"gsx$notes":{"$t":""},"gsx$commentsauthor":{"$t":"Alexa"},"gsx$comment":{"$t":"lacinia mattis. Integer eu lacus. Quisque imperdiet, erat"},"gsx$faces":{"$t":"bobbytwoshoes.jpg"},"gsx$haveseries":{"$t":"0"},"gsx$coverfile":{"$t":"A_Football_Life_Season_One_DVD.jpg"}},{"id":{"$t":"https://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/dnp34"},"updated":{"$t":"2014-05-04T18:30:36.506Z"},"category":[{"scheme":"http://schemas.google.com/spreadsheets/2006","term":"http://schemas.google.com/spreadsheets/2006#list"}],"title":{"type":"text","$t":"Mike And Mike"},"content":{"type":"text","$t":"episodetimingaverage: 240min, description: Mike \u0026 Mike (formerly Mike and Mike in the Morning) is an American sports-talk radio show hosted by Mike Golic and Mike Greenberg on ESPN Radio and simulcast on television, normally on ESPN2.[1] If ESPN is broadcasting a live sporting event during the show's timeslot, SportsCenter will air on ESPN2, and the show's simulcast will therefore then air on ESPNews. If both ESPN and ESPN2 are showing live sporting events, Sportscenter will air on ESPNews and the show will air on either ESPNU or ESPN Classic. The show primarily focuses on the day's biggest sports topics and the humorous banter between the Mikes. On February 24, 2010, the duo celebrated 10 years of doing the show together.[2], coverimage: http://upload.wikimedia.org/wikipedia/en/6/6f/Mike_and_Mike_in_the_Morning_logo.svg, imdbrating: 7,6, broadcast: ESPN, wikipage: http://en.wikipedia.org/wiki/Mike_and_Mike, imdbpage: http://www.imdb.com/title/tt0783392/?ref_\u003dnv_sr_1, tags: News, Sport, Talk-Show\n, originalrun: October 12, 1998 – present, commentsauthor: Francis, comment: Nulla eget metus eu erat semper rutrum. Fusce dolor, faces: justinrgraham.jpg, haveseries: 0, coverfile: Mike_and_Mike_in_the_Morning_logo.svg"},"link":[{"rel":"self","type":"application/atom+xml","href":"http://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/dnp34"}],"gsx$name":{"$t":"Mike And Mike"},"gsx$episodetimingaverage":{"$t":"240min"},"gsx$description":{"$t":"Mike \u0026 Mike (formerly Mike and Mike in the Morning) is an American sports-talk radio show hosted by Mike Golic and Mike Greenberg on ESPN Radio and simulcast on television, normally on ESPN2.[1] If ESPN is broadcasting a live sporting event during the show's timeslot, SportsCenter will air on ESPN2, and the show's simulcast will therefore then air on ESPNews. If both ESPN and ESPN2 are showing live sporting events, Sportscenter will air on ESPNews and the show will air on either ESPNU or ESPN Classic. The show primarily focuses on the day's biggest sports topics and the humorous banter between the Mikes. On February 24, 2010, the duo celebrated 10 years of doing the show together.[2]"},"gsx$source":{"$t":""},"gsx$coverimage":{"$t":"http://upload.wikimedia.org/wikipedia/en/6/6f/Mike_and_Mike_in_the_Morning_logo.svg"},"gsx$imdbrating":{"$t":"7,6"},"gsx$broadcast":{"$t":"ESPN"},"gsx$wikipage":{"$t":"http://en.wikipedia.org/wiki/Mike_and_Mike"},"gsx$imdbpage":{"$t":"http://www.imdb.com/title/tt0783392/?ref_\u003dnv_sr_1"},"gsx$tags":{"$t":"News, Sport, Talk-Show\n"},"gsx$originalrun":{"$t":"October 12, 1998 – present"},"gsx$notes":{"$t":""},"gsx$commentsauthor":{"$t":"Francis"},"gsx$comment":{"$t":"Nulla eget metus eu erat semper rutrum. Fusce dolor"},"gsx$faces":{"$t":"justinrgraham.jpg"},"gsx$haveseries":{"$t":"0"},"gsx$coverfile":{"$t":"Mike_and_Mike_in_the_Morning_logo.svg"}},{"id":{"$t":"https://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/dp3nl"},"updated":{"$t":"2014-05-04T18:30:36.506Z"},"category":[{"scheme":"http://schemas.google.com/spreadsheets/2006","term":"http://schemas.google.com/spreadsheets/2006#list"}],"title":{"type":"text","$t":"SportsCenter"},"content":{"type":"text","$t":"episodetimingaverage: 60 to 120 minutes depending on content, description: A, usually hour-long, sports show featuring all the days major sports news and scores. Sportscenter features many segments of it's own while also taking snippets from other ESPN shows such as 'Pardon the Interuption', College Gameday, NFL Primetime, and NBA Fastbreak. The show also features a top 10 list. As of 2005 Monday through Friday are the top plays of the day. Saturday shows the \"Not Top 10\" and Sunday has Chris Berman's plays of the week, coverimage: http://upload.wikimedia.org/wikipedia/en/d/db/SportsCenter_logo.png, imdbrating: 8,4, broadcast: ESPN, wikipage: http://en.wikipedia.org/wiki/SportsCenter, imdbpage: http://www.imdb.com/title/tt0136668/?ref_\u003dfn_al_tt_1, tags: Sport News, originalrun: September 7, 1979 – present, commentsauthor: Camilla, comment: Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel, faces: derekebradley.jpg, haveseries: 0, coverfile: SportsCenter_logo.png"},"link":[{"rel":"self","type":"application/atom+xml","href":"http://spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/oda/public/values/dp3nl"}],"gsx$name":{"$t":"SportsCenter"},"gsx$episodetimingaverage":{"$t":"60 to 120 minutes depending on content"},"gsx$description":{"$t":"A, usually hour-long, sports show featuring all the days major sports news and scores. Sportscenter features many segments of it's own while also taking snippets from other ESPN shows such as 'Pardon the Interuption', College Gameday, NFL Primetime, and NBA Fastbreak. The show also features a top 10 list. As of 2005 Monday through Friday are the top plays of the day. Saturday shows the \"Not Top 10\" and Sunday has Chris Berman's plays of the week"},"gsx$source":{"$t":""},"gsx$coverimage":{"$t":"http://upload.wikimedia.org/wikipedia/en/d/db/SportsCenter_logo.png"},"gsx$imdbrating":{"$t":"8,4"},"gsx$broadcast":{"$t":"ESPN"},"gsx$wikipage":{"$t":"http://en.wikipedia.org/wiki/SportsCenter"},"gsx$imdbpage":{"$t":"http://www.imdb.com/title/tt0136668/?ref_\u003dfn_al_tt_1"},"gsx$tags":{"$t":"Sport News"},"gsx$originalrun":{"$t":"September 7, 1979 – present"},"gsx$notes":{"$t":""},"gsx$commentsauthor":{"$t":"Camilla"},"gsx$comment":{"$t":"Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin vel"},"gsx$faces":{"$t":"derekebradley.jpg"},"gsx$haveseries":{"$t":"0"},"gsx$coverfile":{"$t":"SportsCenter_logo.png"}}]}};
+var hash = window.location.hash;
+var id = hash.replace("#","");
+
+
+function builder(data) {
+//console.log(data);
+
+    var comments = [];
+    var authors = [];
+    var faces = [];
+    var commentcount = [1,2,3,4,5,6,7,8,9];
+    var commentshtml = [];
+    $(data.feed.entry).each(function(index, feedItem) {
+
+        comments[index] = feedItem['gsx$comment']['$t'];
+        authors[index] = feedItem['gsx$commentsauthor']['$t'];
+        faces[index] = feedItem['gsx$faces']['$t'];
+    });
+
+    //Generatin random comments quaintity
+    var randomcount = commentcount[Math.floor(Math.random()*commentcount.length)];
+
+
+//Generatin block or random comments/authors
+    for (var i = 0; i < randomcount; i++) {
+        var randomcomment = comments[Math.floor(Math.random()*comments.length)];
+        var randomauthor = authors[Math.floor(Math.random()*authors.length)];
+        var randomface = faces[Math.floor(Math.random()*faces.length)];
+        var commentdate = '';
+        if ( i >= 0 && i <=1){
+            commentdate = 'Today';
+        } else if (i >= 2 && i <=3){
+            commentdate = 'Yesterday';
+        } else if (i >= 4 && i <=7){
+            commentdate = '1 Day ago';
+        } else {
+            commentdate = '2 Days ago';
+        }
+        commentshtml.push(
+            '<li>\
+            <img class="avatar" src="items/faces/'+randomface+'" alt="ava"/>\
+                <span class="comment-author">'+randomauthor+'&nbsp;&#8226;&nbsp;</span>'+commentdate+'\
+                <p class="comment">'+randomcomment+'</p>\
+            </li>');
+
+    }
+
+
+    var movie = [];
+
+
+
+        var feedItem = data.feed.entry[id];
+
+        var tags = feedItem['gsx$tags']['$t'];
+        if (tags !=''){
+            tags = '<span class="tag"><a class="brown" href="#tag1">' + tags.replace(/,/gi,'</a></span><span class="tag"><a class="brown" href="#tag2">') + '</a></span>';
+        }
+        var name = feedItem['gsx$name']['$t'];
+        var imageLink = feedItem['gsx$coverfile']['$t'];
+        var imdb =  feedItem['gsx$imdbrating']['$t'];
+        var description = feedItem['gsx$description']['$t'];
+        var broadcast = feedItem['gsx$broadcast']['$t'];
+        var haveseries = feedItem['gsx$haveseries']['$t'];
+
+    var series = '';
+
+    if (haveseries == 1){
+        series = '<ul class="episodes cycle-slideshow" data-cycle-fx="carousel" data-cycle-timeout="0" data-cycle-slides="> li" data-cycle-swipe="true">' +
+            '<li><img  class="img-responsive" src="items/covers/'+imageLink+'"><h6>Episode 1</h6></li>' +
+            '<li><img  class="img-responsive" src="items/covers/'+imageLink+'"><h6>Episode 2</h6></li>' +
+            '<li><img  class="img-responsive" src="items/covers/'+imageLink+'"><h6>Episode 3</h6></li>' +
+            '<li><img  class="img-responsive" src="items/covers/'+imageLink+'"><h6>Episode 4</h6></li>' +
+            '<li><img  class="img-responsive" src="items/covers/'+imageLink+'"><h6>Episode 5</h6></li>' +
+                  '</ul>';
+
+    }else{
+        series = '';
+    }
+
+    movie.push(
+            '<div class="col-xs-12 col-sm-6 col-md-4">\
+                <div class="movie-item">\
+                   <div class="item-body">\
+                              <img  class="img-responsive" src="items/covers/'+imageLink+'">\
+                             <a class="play" href="http://nebel.tv/wrapper/getVideoAssets?id=0#play"><span class="glyphicon glyphicon-play"></span></a>\
+                    </div>\
+                    <div class="item-footer">\
+                        <div class="video-title row">\
+                        <div class="col-xs-10 title-holder">\
+                            <span class="item-title">'+name+'</span>'+tags+'\
+                        </div>\
+                        <div class="col-xs-2">\
+                           <span class="connection-speed "> </span>\
+                        </div>\
+                       </div>\
+                       <div class="video-social">\
+                       <span class="item-social"><span class="icon icon-like"></span>'+(Math.floor(Math.random()*10))+'   <span class="icon icon-comments"></span>'+randomcount+'</span>\
+                       </div>\
+                       \
+                           <span class="item-author" style="background: url(images/hromadske-icon.png) left no-repeat;">'+broadcast+' </span>\
+                       <a class="watch-later">+ Watch Later</a>\
+                       <div class="clearfix"></div>\
+                    </div>\
+                   '+series+'\
+                    <div class="description">\
+                        <h4>Details</h4>\
+                        <span>IMDB Rating:</span><br>\
+                        <span class="rating">\
+                            <span class="glyphicon glyphicon-star"></span>\
+                            <span class="glyphicon glyphicon-star"></span>\
+                            <span class="glyphicon glyphicon-star"></span>\
+                            <span class="glyphicon glyphicon-star"></span>\
+                            <span class="glyphicon glyphicon-star-empty"></span>\
+                            '+imdb+'</span>\
+                                <div class="text">\
+                                    <p>'+description+'</p>\
+                                    <div class="shadow-mask"></div>\
+                                </div>\
+                            <a class="showmore text-right" href="#"><span>Show More</span></a>\
+                         </div>\
+                    <div class="comments">\
+                    <h4>Comments&nbsp;<span class="count">'+randomcount+'</span></h4>\
+                    <ul id="comments-list"> </ul>\
+                    </div>\
+                </div>\
+            </div>'
+        );
+
+
+
+
+
+
+
+    $('#movie-overview').append(movie);
+    $('#comments-list').append(commentshtml);
+
+
+    $('.dots').click(function(e){
+        e.preventDefault();
+        $(this).next('.more-dots').toggle('slow');
+    });
+    var textheight = $('.text p').height();
+        console.log(textheight);
+    if(textheight > 75) {
+        $('.text p').css('height', 100);
+
+        $('.showmore, .shadow-mask').css('display', 'block');
+    }
+    $('.showmore').click(function(e){
+        e.preventDefault();
+        $('.text p').css('height', 'auto');
+        $(this).hide();
+            $('.shadow-mask').hide();
+    });
+    $('.cycle-slideshow').cycle();
+}
+
+
+$(document).ready(function(){
+
+
+   // $.getJSON("http://cors.io/spreadsheets.google.com/feeds/list/0AirkLgmZ4th5dHlHeVJBZGlWQjBGTFprM0gzT1pKamc/od6/public/values?alt=json", builder);
+
+builder(data);
+
+});
